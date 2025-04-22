52a437301fa00bb542bc9c9700432b510b9254a0
u112 <u112@d.c>
2025-04-23 02:11:13 +0300

    Fixed issues with posts for users

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blogs.Controllers
 {
-    [Authorize] // Только авторизованные могут создавать посты
+    [Authorize] 
     public class PostsController : Controller
     {
         private readonly IRepository _repo;
@@ -18,15 +18,6 @@ namespace Blogs.Controllers
             _userManager = userManager;
         }
 
-        //public async Task<IActionResult> Index()
-        //{
-        //    //var posts = _repo.GetAllPosts();
-        //    //return View(posts);
-        //    var user = await _userManager.GetUserAsync(User);
-        //    var posts = _repo.GetAllPosts().Where(p => p.UserId == user.Id).ToList(); // Только свои посты
-        //    return View(posts);
-
-        //}
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
@@ -44,25 +35,6 @@ namespace Blogs.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create(Post post)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = await _userManager.GetUserAsync(User);
-        //        post.UserId = user.Id;
-
-        //        _repo.AddPost(post);
-        //        await _repo.SaveChangesAsync();
-
-        //        return RedirectToAction("Index", "Profile"); // или Index
-        //    }
-
-        //    return View(post);
-        //}
-
-        [HttpPost]
         [HttpPost]
         public async Task<IActionResult> Create(Post post)
         {
@@ -82,8 +54,6 @@ namespace Blogs.Controllers
             }
             return View(post);
         }
-
-
 
     }
 }
